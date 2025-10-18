0cc16774edd2687819681558f741eeafed50fa36
smonasterio <sofimonasterio93@gmail.com>
2025-10-18 14:03:06 -0300

    ejercicio 7

--- src/tp5/TP5.java
+++ src/tp5/TP5.java
@@ -4,9 +4,9 @@
  */
 package tp5;
 
-import tp5.ejercicio6.Cliente;
-import tp5.ejercicio6.Mesa;
-import tp5.ejercicio6.Reserva;
+import tp5.ejercicio7.Conductor;
+import tp5.ejercicio7.Motor;
+import tp5.ejercicio7.Vehiculo;
 
 /**
  *
@@ -18,6 +18,7 @@ public class TP5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //EJERCICIO 1
         /*Foto foto = new Foto("foto_sofia.jpg", "JPEG");
         Pasaporte pasaporte = new Pasaporte("AR123456", "20/10/2024", foto);
         Titular titular = new Titular("Sofia", "12.123.123");
@@ -27,7 +28,8 @@ public class TP5 {
         System.out.println(pasaporte);
         System.out.println(titular.getPasaporte());*/
 
- /*Bateria bateria = new Bateria("BL-45F1F", 3000);
+        //EJERCICIO 2
+        /*Bateria bateria = new Bateria("BL-45F1F", 3000);
         Celular celular = new Celular("357894561234567", "LG", "K10", bateria);
         Usuario usuario = new Usuario("Sofia", "12.123.123");
 
@@ -35,13 +37,15 @@ public class TP5 {
 
         System.out.println(celular);
         System.out.println(usuario.getCelular());*/
- /*Autor autor = new Autor("Gabriel García Márquez", "Colombiana");
+        //EJERCICIO 3
+        /*Autor autor = new Autor("Gabriel García Márquez", "Colombiana");
         Editorial editorial = new Editorial("Sudamericana", "Buenos Aires, Argentina");
 
         Libro libro = new Libro("Cien Años de Soledad", "978-9871138663", autor, editorial);
 
         System.out.println(libro);*/
- /*Banco banco = new Banco("Banco Nación", "30-50000123-9");
+        //EJERCICIO 4
+        /*Banco banco = new Banco("Banco Nación", "30-50000123-9");
         TarjetaDeCredito tarjeta = new TarjetaDeCredito("1234-5678-9012-3456", "12/28", banco);
         Cliente cliente = new Cliente("Sofia", "12.123.123");
 
@@ -49,18 +53,29 @@ public class TP5 {
         
         System.out.println(tarjeta);
         System.out.println(cliente.getTarjeta());*/
- /*Computadora compu = new Computadora("HP", "SN12345", "ASUS PRIME B450M", "AMD B450");
+        //EJERCICIO 5
+        /*Computadora compu = new Computadora("HP", "SN12345", "ASUS PRIME B450M", "AMD B450");
         Propietario propietario = new Propietario("Sofia", "12.123.123");
 
         compu.setPropietario(propietario);
 
         System.out.println(compu);
         System.out.println(propietario.getComputadora());*/
-        Cliente cliente = new Cliente("Juan Pérez", "3815123456");
+        //EJERCICIO 6
+        /*Cliente cliente = new Cliente("Juan Pérez", "3815123456");
         Mesa mesa = new Mesa(7, 4);
         Reserva reserva = new Reserva("2025-10-18", "21:00", cliente, mesa);
 
-        System.out.println(reserva);
+        System.out.println(reserva);*/
+        //EJERCICIO 7
+        Motor motor = new Motor("V8", "MTR-5567");
+        Vehiculo vehiculo = new Vehiculo("AB123CD", "Toyota Corolla", motor);
+        Conductor conductor = new Conductor("Laura Gómez", "LIC-998877");
+
+        vehiculo.setConductor(conductor);
+
+        System.out.println(vehiculo);
+        System.out.println(conductor);
     }
 
 }
