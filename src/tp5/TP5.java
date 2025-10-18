3058e09517bb60636740daac7ada53d3418ad5a3
smonasterio <sofimonasterio93@gmail.com>
2025-10-18 13:06:58 -0300

    ejercicio 2

--- src/tp5/TP5.java
+++ src/tp5/TP5.java
@@ -4,10 +4,9 @@
  */
 package tp5;
 
-import java.util.Date;
-import tp5.ejercicio1.Foto;
-import tp5.ejercicio1.Pasaporte;
-import tp5.ejercicio1.Titular;
+import tp5.ejercicio2.Bateria;
+import tp5.ejercicio2.Celular;
+import tp5.ejercicio2.Usuario;
 
 /**
  *
@@ -19,14 +18,23 @@ public class TP5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Foto foto = new Foto("foto_sofia.jpg", "JPEG");
+        /*Foto foto = new Foto("foto_sofia.jpg", "JPEG");
         Pasaporte pasaporte = new Pasaporte("AR123456", "20/10/2024", foto);
         Titular titular = new Titular("Sofia", "12.123.123");
 
         pasaporte.setTitular(titular);
 
         System.out.println(pasaporte);
-        System.out.println(titular.getPasaporte());
+        System.out.println(titular.getPasaporte());*/
+        
+        Bateria bateria = new Bateria("BL-45F1F", 3000);
+        Celular celular = new Celular("357894561234567", "LG", "K10", bateria);
+        Usuario usuario = new Usuario("Sofia", "12.123.123");
+
+        celular.setUsuario(usuario);
+
+        System.out.println(celular);
+        System.out.println(usuario.getCelular());
     }
 
 }
---
+++ src/tp5/ejercicio2/Celular.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp5.ejercicio2;
+
+/**
+ *
+ * @author sofim
+ */
+public class Celular {
+
+    private String imei;
+    private String marca;
+    private String modelo;
+    private Bateria bateria;
+    private Usuario usuario;
+
+    public Celular(String imei, String marca, String modelo, Bateria bateria) {
+        this.imei = imei;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.bateria = bateria;
+    }
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Bateria getBateria() {
+        return bateria;
+    }
+
+    public void setBateria(Bateria bateria) {
+        this.bateria = bateria;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+        if (usuario.getCelular() != this) {
+            usuario.setCelular(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Celular: imei=" + imei + ", marca=" + marca + ", modelo=" + modelo
+                + ", bateria=" + bateria + ", usuario=" + usuario;
+    }
+}
